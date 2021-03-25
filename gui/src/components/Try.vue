e2ea99fc997d95da3dd15941a744dbbc4ead1b6f
krizada1 <davidkrizan1997@gmail.com>
2021-03-25 12:58:47 +0100

    Try.Vue added (D1 move control component)

---
+++ gui/src/components/Try.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fluid>
+    <v-switch
+      v-model="switch1"
+      :label="`D1 Move: ${switch1.toString()}`"
+    ></v-switch>
+  </v-container>
+  
+</template>
+<script>
+  export default {
+      name: "Try",
+    
+    data () {
+      return {
+        switch1: true,
+      }
+    },
+  }
+</script>
